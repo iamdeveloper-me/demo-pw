9f19ad2d49fa4c759c8025a09f90eb330854b96c
hp <iamdeveloper.me@gmail.com>
2018-06-30 11:57:28 +0530

    login register with js & jquery

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit , Input} from '@angular/core';
+import { NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+
+import {
+  AuthService,
+  FacebookLoginProvider,
+  GoogleLoginProvider
+} from 'angular5-social-login';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
-})
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss']
+  })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
-
   ngOnInit() {
+    $.getScript('./assets/js/hideshow.js');
+  }
+  constructor( private socialAuthService: AuthService ,private modalService: NgbModal ) {}
+  closeResult: string;
+  // Open default modal
+  open(content) {
+      this.modalService.open(content).result.then((result) => {
+          this.closeResult = `Closed with: ${result}`;
+      }, (reason) => {
+          this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+      });
+  }
+  // This function is used in open
+  private getDismissReason(reason: any): string {
+      if (reason === ModalDismissReasons.ESC) {
+          return 'by pressing ESC';
+      } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+          return 'by clicking on a backdrop';
+      } else {
+          return `with: ${reason}`;
+      }
   }
 
+  // Open modal with dark section
+  openModal(customContent) {
+      this.modalService.open(customContent, { windowClass: 'dark-modal' });
+  }
+
+    public socialSignIn(socialPlatform : string) {
+      let socialPlatformProvider;
+      if(socialPlatform == "facebook"){
+          socialPlatformProvider = FacebookLoginProvider.PROVIDER_ID;
+      }else if(socialPlatform == "google"){
+          socialPlatformProvider = GoogleLoginProvider.PROVIDER_ID;
+      }
+
+      this.socialAuthService.signIn(socialPlatformProvider).then(
+          (userData) => {console.log(socialPlatform+" sign in data : " , userData)
+          }
+      );
+    }
 }

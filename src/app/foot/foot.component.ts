5180c4077dd4d7ebd69a01624d279d3e423a7de8
saurabh <saurabh@thoughtwin.com>
2018-07-03 19:29:28 +0530

    footer colasping

--- src/app/foot/foot.component.ts
+++ src/app/foot/foot.component.ts
@@ -10,6 +10,13 @@ export class FootComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
+
+    $(".footer_bg button.btn.btn-info").click(function(){
+      $(this).toggleClass("hide_menus");
+  });
+
+    
   }
 
 }

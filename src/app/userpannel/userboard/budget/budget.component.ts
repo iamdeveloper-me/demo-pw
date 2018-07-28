7172e6fb916b1bf8fe5006e00521059f4e196a68
saurabh <saurabh@thoughtwin.com>
2018-07-28 16:54:17 +0530

    budget page create

--- src/app/userpannel/userboard/budget/budget.component.ts
+++ src/app/userpannel/userboard/budget/budget.component.ts
@@ -13,6 +13,27 @@ export class BudgetComponent implements OnInit {
     
     $("li").removeClass("user");
     $("#login").hide();
+
+
+    var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
+
+        /* Toggle between hiding and showing the active panel */
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
+
   }
 
 }
--- src/app/userpannel/userboard/vendorlist/vendorlist.component.ts
+++ src/app/userpannel/userboard/vendorlist/vendorlist.component.ts
@@ -20,14 +20,14 @@ export class VendorlistComponent implements OnInit {
         if(value == "all")
         {
             //$('.filter').removeClass('hidden');
-            $('.filter').show('1000');
+            $('.filter').show(1000);
         }
         else
         {
 //            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
 //            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
-            $(".filter").not('.'+value).hide('3000');
-            $('.filter').filter('.'+value).show('3000');
+            $(".filter").not('.'+value).hide(3000);
+            $('.filter').filter('.'+value).show(3000);
             
         }
 

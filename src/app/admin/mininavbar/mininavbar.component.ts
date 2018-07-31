6f0fc4a8b49227a5b6fc8b6491c6af30a5fed0d2
iamdeveloper-me <iamdeveloper.me@gmail.com>
2018-07-31 15:30:40 +0530

    admin setup

---
+++ src/app/admin/mininavbar/mininavbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mininavbar',
+  templateUrl: './mininavbar.component.html',
+  styleUrls: ['./mininavbar.component.scss']
+})
+export class MininavbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/admin/useraccountlist/useraccountlist.component.ts
+++ src/app/admin/useraccountlist/useraccountlist.component.ts
@@ -1,6 +1,30 @@
-import { Component, OnInit ,ViewEncapsulation } from '@angular/core';
+import { Component, OnInit ,ViewEncapsulation ,Input} from '@angular/core';
 import * as tableData from '../../shared/data/smart-data-table';
 import { LocalDataSource } from 'ng2-smart-table';
+import { NgbModal, ModalDismissReasons, NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+    selector: 'ngbd-modal-content',
+    template: `
+    <div class="modal-header">
+      <h4 class="modal-title">Hi there!</h4>
+      <button type="button" class="close" aria-label="Close" (click)="activeModal.dismiss('Cross click')">
+        <span aria-hidden="true">&times;</span>
+      </button>
+    </div>
+    <div class="modal-body">
+      <p>Hello, {{name}}!</p>
+    </div>
+    <div class="modal-footer">
+      <button type="button" class="btn btn-secondary btn-raised" (click)="activeModal.close('Close click')">Close</button>
+    </div>
+  `
+})
+
+export class NgbduserModalContent {
+    @Input() name;
+    constructor(public activeModal: NgbActiveModal) { }
+}
 
 @Component({
   selector: 'app-useraccountlist',
@@ -25,7 +49,7 @@ export class UseraccountlistComponent implements OnInit {
   filterSource: LocalDataSource;
   alertSource: LocalDataSource;
 
-  constructor() {
+  constructor(private modalService: NgbModal) {
       this.source = new LocalDataSource(tableData.data); // create the source
       this.filterSource = new LocalDataSource(tableData.filerdata); // create the source
       this.alertSource = new LocalDataSource(tableData.alertdata); // create the source
@@ -89,5 +113,40 @@ export class UseraccountlistComponent implements OnInit {
       } else {
           event.confirm.reject();
       }
+  }
+  /////////////////////////////////popupmodel
+
+  closeResult: string;
+
+
+  // Open default modal
+  open(content) {
+      this.modalService.open(content).result.then((result) => {
+          this.closeResult = `Closed with: ${result}`;
+      }, (reason) => {
+          this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+      });
+  }
+
+  // This function is used in open
+  private getDismissReason(reason: any): string {
+      if (reason === ModalDismissReasons.ESC) {
+          return 'by pressing ESC';
+      } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+          return 'by clicking on a backdrop';
+      } else {
+          return `with: ${reason}`;
+      }
+  }
+
+  // Open modal with dark section
+  openModal(customContent) {
+      this.modalService.open(customContent, { windowClass: 'dark-modal' });
+  }
+
+  // Open content with dark section
+  openContent() {
+      const modalRef = this.modalService.open( NgbduserModalContent);
+      modalRef.componentInstance.name = 'World';
   }
 }

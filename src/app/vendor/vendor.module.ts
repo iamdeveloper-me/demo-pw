68c5a64bce59f41ceac604a875b5c68eb64814ef
iamdeveloper-me <iamdeveloper.me@gmail.com>
2018-08-08 21:03:58 +0530

    build changes

--- src/app/vendor/vendor.module.ts
+++ src/app/vendor/vendor.module.ts
@@ -26,7 +26,7 @@ import { MatchHeightModule } from "../shared/directives/match-height.directive";
 // import { MessageComponent } from './message/message.component';
  import { ReviewComponent } from './review/review.component';
  import { EventComponent } from './event/event.component';
-import { EventListComponent } from './event-list/event-list.component';
+//import { EventListComponent } from './event-list/event-list.component';
 
 
 //import { PortfolioviewComponent } from './portfolioview/portfolioview.component';
@@ -74,7 +74,7 @@ import { EventListComponent } from './event-list/event-list.component';
         // MessageComponent,
          ReviewComponent,
          EventComponent,
-         EventListComponent,
+        // EventListComponent,
 
         // PortfolioviewComponent,
         // ViewPhotoAlbumsComponent,

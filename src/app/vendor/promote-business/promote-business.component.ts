0d06cc858439a3316f30eb3a8e6156f6afc49d07
pankaj <pankaj@thoughtwin.com>
2018-08-20 21:25:37 +0530

    promote business

--- src/app/vendor/promote-business/promote-business.component.ts
+++ src/app/vendor/promote-business/promote-business.component.ts
@@ -55,6 +55,44 @@ export class PromoteBusinessComponent implements OnInit {
     $(".close").click(function(){
         $(".alert").hide();
      });
+
+     $(".gallery").click(function(){
+        $(".homegallerybox").show();
+        $(".prioritybox").hide();
+        $(".audiencebox").hide();
+        $(".dealsbox").hide();
+        $(".homebannerbox").hide();
+     });
+      $(".priority").click(function(){
+        $(".homegallerybox").hide();
+        $(".prioritybox").show();
+        $(".audiencebox").hide();
+        $(".dealsbox").hide();
+        $(".homebannerbox").hide();
+     });
+      $(".audience").click(function(){
+        $(".homegallerybox").hide();
+        $(".prioritybox").hide();
+        $(".audiencebox").show();
+        $(".dealsbox").hide();
+        $(".homebannerbox").hide();
+     });
+      $(".deals").click(function(){
+        $(".homegallerybox").hide();
+        $(".prioritybox").hide();
+        $(".audiencebox").hide();
+        $(".dealsbox").show();
+        $(".homebannerbox").hide();
+     });
+      $(".homebanner").click(function(){
+        $(".homegallerybox").hide();
+        $(".prioritybox").hide();
+        $(".audiencebox").hide();
+        $(".dealsbox").hide();
+        $(".homebannerbox").show();
+     });
+
+
   }
 
   closeResult: string;

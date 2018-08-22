ecef82b79f573da9bc425ddf360e1e1109b85fd1
saurabh <saurabh@thoughtwin.com>
2018-08-22 20:08:16 +0530

    promotion page creat

---
+++ src/app/vendor/promotion-priority/promotion-priority.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-promotion-priority',
+  templateUrl: './promotion-priority.component.html',
+  styleUrls: ['./promotion-priority.component.scss']
+})
+export class PromotionPriorityComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

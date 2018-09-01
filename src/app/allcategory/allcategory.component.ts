58d581fc94e37ef3245c1aaa371e955eef8b2b51
pankaj <pankaj@thoughtwin.com>
2018-09-01 13:35:07 +0530

    Paralax effect on all category page

--- src/app/allcategory/allcategory.component.ts
+++ src/app/allcategory/allcategory.component.ts
@@ -11,7 +11,22 @@ export class AllcategoryComponent implements OnInit {
 
   ngOnInit() {
  	$.getScript('./assets/js/stopExecutionOnTimeout.js');
- 	$.getScript('./assets/js/aos.js');
+
+
+ 	$(function(){
+	$(window).scroll( function(){
+		
+		var bottom_of_window = $(window).scrollTop() + $(window).height();
+		//fade-in
+		$('.fade-ani').each(function(){
+			var bottom_of_object = $(this).position().top + $(this).outerHeight();
+			 if( bottom_of_window > bottom_of_object ){
+				$(this).addClass('showing');
+			}
+			else{
+				$(this).removeClass('showing');
+			}
+		});
   }
 
 

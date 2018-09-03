88ab562b610206804a33b314e685c6251efb574f
Saurabh <iamdeveloper.me@gmail.com>
2018-09-03 11:27:58 +0530

    update user ui

---
+++ src/app/userpannel/userboard/usermailsearch/usermailsearch.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-usermailsearch',
+  templateUrl: './usermailsearch.component.html',
+  styleUrls: ['./usermailsearch.component.scss']
+})
+export class UsermailsearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

e04c91ffd6db151843821712fe4197824dfce761
pankaj <pankaj@thoughtwin.com>
2018-09-28 17:28:46 +0530

    ui changes pankaj

--- src/app/admin/adminexpenses/adminexpenses.component.ts
+++ src/app/admin/adminexpenses/adminexpenses.component.ts
@@ -10,6 +10,15 @@ export class AdminexpensesComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  $.getScript('https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js');
+
+  $.getScript('https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js');
+  $.getScript('https://use.fontawesome.com/4e4388f72e.js');
+  $.getScript('./assets/js/curosselfun.js');
+  
+
+
+
   }
 
 }
--- src/assets/js/curosselfun.js
+++ src/assets/js/curosselfun.js
@@ -386,3 +386,24 @@ $(".wedding-icon4").click(function() {
     $('.wedding-icon2').removeClass('btnactive');
     $('.wedding-icon3').removeClass('btnactive');
 });
+
+
+window.onload = function(){
+    function readURL(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            
+            reader.onload = function(e) {
+               document.querySelector('.img-preview').setAttribute('src', e.target.result);
+            };
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+    
+    var imageInput = document.querySelector('#logo-id');
+    imageInput.onchange=changeEventHandler;
+    
+    function changeEventHandler(event) {
+        readURL(this);
+    }
+};

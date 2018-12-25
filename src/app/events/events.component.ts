466d8bbf89fda454972ef9247523f53a1f005819
Pankaj <iamdeveloper-me@gmail.com>
2018-12-25 14:36:41 +0530

    Page

--- src/app/events/events.component.ts
+++ src/app/events/events.component.ts
@@ -18,7 +18,7 @@ export class EventsComponent implements OnInit {
 
   pageSize:number = 10000
   // array of all items to be paged
- private allItems: any[];
+ // private allItems: any[];
   total_item_page
   page_sizzze  = 1;
   pager: any = {};
@@ -34,7 +34,28 @@ export class EventsComponent implements OnInit {
     this.location();
   }
   page2 = 4;
- 
+  event(list){
+    console.log(list.value);
+  
+
+    this.apiService.postData(this.apiService.serverPath+'Home/searchevents',{
+      page: 0,
+      pageSize: this.pagesSelected,
+      sortDir: "",
+      sortedBy: "asc",
+      searchQuery: "",
+      location: "",
+      eventType: "Free",
+      dates: "All"
+    }).subscribe(data => {
+          
+          this.searchevents = data
+          console.log( this.searchevents)
+
+          this.setPage(1 ,this.searchevents.totalPages);
+        });
+    this.location();
+  }
 
 
   setPage(page: number,a) {
@@ -47,27 +68,7 @@ export class EventsComponent implements OnInit {
     this.pagedItems = a.slice(this.pager.startIndex, this.pager.endIndex + 1);
   }
   
-  event(list){
-        console.log(list.value);
-        this.apiService.postData(this.apiService.serverPath+'Home/searchevents',{
-          page: 0,
-          pageSize: list.value.pageSize,
-          sortDir: "",
-          sortedBy: "asc",
-          searchQuery: list.value.searchQuery,
-          location: list.value.location,
-          eventType: list.value.eventType,
-          dates:list.value.dates
-        }).subscribe(data => {
-              this.searchevents = data ;
-              // set items to json response
-              console.log(data)
-              this.total_item_page = data.totalPages;
-            
-              // initialize to page 1
-              this.setPage(1,this.total_item_page);
-            },error => {console.log(error)});
-  }
+
 
   location(){ 
     this.masterservice.getAllLocation().subscribe(data => {

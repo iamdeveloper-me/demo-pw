d23bcf32f4dde8aeabfbe47ee11ee7a4efa9f95d
mahima <mahima@gmail.com>
2018-12-31 12:31:23 +0530

    add all

--- src/app/vendor/gallery/gallery.component.ts
+++ src/app/vendor/gallery/gallery.component.ts
@@ -8,7 +8,7 @@ import { apiService } from '../../shared/service/api.service';
 })
 
 export class GalleryComponent implements OnInit {
-  // $.getScript('./assets/js/vendorsidebar.js');
+ 
   @ViewChild("fileInput") fileInput;
   gallery = { files: ''}
   fileToUpload:any;
@@ -24,83 +24,67 @@ export class GalleryComponent implements OnInit {
   albumArray:any = [];
   albumArray_length;
   private albumget: string  = 'http://testapp-env.tyad3n63sa.ap-south-1.elasticbeanstalk.com/api/Albums/myalbums'
-private getportfolio: string  = 'http://testapp-env.tyad3n63sa.ap-south-1.elasticbeanstalk.com/api/Supplier/myportfolio'
-
-
-
-  constructor(
-    public http: Http,
-    private apiService : apiService,
-  ) { }
-  ngOnInit() {
-  
-       $.getScript('./assets/js/vendorsidebar.js');
-
-    let headers = new Headers();
-    var authToken = localStorage.getItem('userToken');
-    
-    headers.append('Accept', 'application/json')
-    headers.append('Content-Type', 'application/json');
-    headers.append("Authorization",'Bearer '+authToken);
-    var basicplan = localStorage.getItem('basic-plan');
-    this.basicplane =  localStorage.getItem('basic-plan');
-      console.log(parseInt(basicplan) );
-      if( parseInt(basicplan) == 1 ){
-       
-        $(".albumlist").hide();
-      }else{
-        $('div').removeClass("overlay");
+  private getportfolio: string  = 'http://testapp-env.tyad3n63sa.ap-south-1.elasticbeanstalk.com/api/Supplier/myportfolio'
+  constructor(public http: Http,private apiService : apiService) { }
+      ngOnInit() {
       
+        $.getScript('./assets/js/vendorsidebar.js');
+        let headers = new Headers();
+        var authToken = localStorage.getItem('userToken');
+        headers.append('Accept', 'application/json')
+        headers.append('Content-Type', 'application/json');
+        headers.append("Authorization",'Bearer '+authToken);
+        var basicplan = localStorage.getItem('basic-plan');
+        this.basicplane = parseInt(localStorage.getItem('basic-plan')) 
+          // if( parseInt(basicplan) == 1 ){
+          //   $(".albumlist").hide();
+          // }else{
+          //   $('div').removeClass("overlay");
+          // }
+        
+        
+        
+        this.showport();
+
+        this.showalbum();
       }
-    
-      this.basicplane = parseInt(localStorage.getItem('basic-plan')) 
-    
-    this.showport();
-
-    this.showalbum();
-  }
-
-        showport()
-        {            
-           
-          //poryfolio get
-
+      showport(){            
+        //poryfolio get
           this.apiService.getData(this.getportfolio).subscribe(res =>{
-            this.portfolio = res;
-            this.portArray = res;
-            this.portArray_length = this.portArray.length 
-            },
-            error => { console.log('aaaaaaaaaaa',error)
-          }
-          )
+          this.portfolio = res;
+          this.portArray = res;
+          this.portArray_length = this.portArray.length 
+          },
+          error => { console.log('aaaaaaaaaaa',error)
         }
-
-        showalbum(){ 
-                      
-                      let headers = new Headers();
-                      var authToken = localStorage.getItem('userToken');
-                    
-                      headers.append('Accept', 'application/json')
-                      headers.append('Content-Type', 'application/json');
-                      headers.append("Authorization",'Bearer '+authToken);
-                      
-                      
-                      this.http.get(this.albumget,{headers:headers}).subscribe(data =>{  
-                      this.albumArray = data.json() ;
-                      this.albumArray_length = this.albumArray.length
-                    })
-                 }
-detectFiles(event) {
-  this.urls = [];
-  let files = event.target.files;
-  if (files) {
-    for (let file of files) {
-      let reader = new FileReader();
-      reader.onload = (e: any) => {
-        this.urls.push(e.target.result);
+        )
       }
-      reader.readAsDataURL(file);
-    }
-  }
-}
+      showalbum(){ 
+                    
+                    let headers = new Headers();
+                    var authToken = localStorage.getItem('userToken');
+                  
+                    headers.append('Accept', 'application/json')
+                    headers.append('Content-Type', 'application/json');
+                    headers.append("Authorization",'Bearer '+authToken);
+                    
+                    
+                    this.http.get(this.albumget,{headers:headers}).subscribe(data =>{  
+                    this.albumArray = data.json() ;
+                    this.albumArray_length = this.albumArray.length
+                  })
+      }
+      detectFiles(event) {
+        this.urls = [];
+        let files = event.target.files;
+        if (files) {
+          for (let file of files) {
+            let reader = new FileReader();
+            reader.onload = (e: any) => {
+              this.urls.push(e.target.result);
+            }
+            reader.readAsDataURL(file);
+          }
+        }
+      }
   }

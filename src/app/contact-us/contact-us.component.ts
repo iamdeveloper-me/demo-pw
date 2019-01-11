6b572326562641a5dd72a5cec429fb01110ca542
amit <amitmalviya373@gmail.com>
2019-01-11 12:48:06 +0530

    add alll class changes

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -1,34 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { apiService } from '../shared/service/api.service';
 import { ToastrService } from 'ngx-toastr';
+import { ContactUsVM } from '../advertise/advertise.component';
 @Component({
   selector: 'app-contact-us',
   templateUrl: './contact-us.component.html',
   styleUrls: ['./contact-us.component.scss']
 })
 export class ContactUsComponent implements OnInit {
- 
-  constructor(private apiService: apiService, public toastr: ToastrService) { }
-
-    name: "string";
-    email: "string";
-    subject: "string";
-    phoneNumber: "string";
-    message: "string";
-    messageType = 1;
-    Phone_no ;
+  contactus: ContactUsVM;
+  constructor(private apiService: apiService, public toastr: ToastrService ) {
+    this.contactus = new ContactUsVM();
+  }
+  Phone_no ;
   ngOnInit() {
-    //this.contactus(c)
   }
   contact(list){
-    console.log(list.value)
     this.apiService.postData(this.apiService.serverPath+'Home/contactus',list.value).subscribe(data => {
-      console.log(data)
       this.toastr.success(data.message);
-      list.reset()
+      list.reset();
     },
       error => {
-       console.log(error)
        this.toastr.error(error._body.split('[')[1].split(']')[0]);
       }
     )
@@ -45,12 +37,6 @@ export class ContactUsComponent implements OnInit {
 
 
 
-  // messageType =1
-  // name: "string"
-  // email: "string"
-  // subject: "string"
-  // phoneNumber: "string"
-  // message: "string"
-  // Phone_no ;
+
 
   

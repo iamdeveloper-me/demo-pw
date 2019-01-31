ff5c682cf4d2b5a27bb7c080c0092d0f93e0bd62
mahima <mahima@gmail.com>
2019-01-31 20:05:08 +0530

    photo l

--- src/app/photogallerydetail/photogallerydetail.component.ts
+++ src/app/photogallerydetail/photogallerydetail.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class PhotogallerydetailComponent implements OnInit {
 
   constructor() { }
-  vendorDetails = {albums:[]};
+  vendorDetails = {};
   portpholio = [];
   portfolios_length:any;
   ngOnInit() {

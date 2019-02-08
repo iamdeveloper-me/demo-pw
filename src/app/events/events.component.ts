1393f075837f3b7289ec37aaa3920961377389cf
amit <amitmalviya373@gmail.com>
2019-02-08 19:22:26 +0530

    Events Changes

--- src/app/events/events.component.ts
+++ src/app/events/events.component.ts
@@ -69,6 +69,7 @@ export class EventsComponent implements OnInit {
       eventType: list?list.value.eventType:'all',
       dates: list?list.value.dates:'all'
     }
+    //Event API
     this.http.post(this.apiService.serverPath+'PerfectWedding/searchevents',q).map((response: Response) => response.json()).subscribe(data => {
         this.allItems = data['items'];
         console.log(this.allItems);

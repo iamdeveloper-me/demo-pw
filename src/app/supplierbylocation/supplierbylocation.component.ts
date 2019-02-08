89e7d16e3d287a31150d7da2d5bc45144b4544dc
amit <amitmalviya373@gmail.com>
2019-02-08 12:19:13 +0530

    Deal Deatail EventList Events SupplierByLocation

--- src/app/supplierbylocation/supplierbylocation.component.ts
+++ src/app/supplierbylocation/supplierbylocation.component.ts
@@ -55,7 +55,7 @@ export class SupplierbylocationComponent implements OnInit {
       this.objFilterParam.locationId = var_data['location'];
     }
       sessionStorage.setItem('filterParam',JSON.stringify(this.objFilterParam));
-      this.router.navigate(['home/weddingvendors',this.objFilterParam.categoryName.replace(/\s/g,'')]);
+      this.router.navigate(['home/weddingvendors']);
 }
 
 }

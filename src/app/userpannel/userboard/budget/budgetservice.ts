bd11d1b22853c012fbb3eb7b8dcfad67365498d7
Pankaj <iamdeveloper-me@gmail.com>
2019-02-28 19:24:27 +0530

    remove debugger

--- src/app/userpannel/userboard/budget/budgetservice.ts
+++ src/app/userpannel/userboard/budget/budgetservice.ts
@@ -23,7 +23,6 @@ ngOnInit(){
 
 }
 public createUpdateBudgetItem(): Observable<any> {
-    debugger
    return this.apiservice.postData(this.apiservice.serverPath+ this.budgetItemApi_url,this.objBudgetItem)
 }
 public createUpdateBudgetCategories(){

288802e1856500b328f253cf7d01278aeb6dfaa8
Pankaj <iamdeveloper-me@gmail.com>
2019-03-04 18:19:12 +0530

    new build vhanges

---
+++ dist/assets/js/hideshow.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+    // $("#hide").click(function(){
+    //     $("#login").hide();
+    // });
+
+    $("#new").click(function(){
+        $("#signupheading").show();
+        $("#signupform").show();
+        $("#loginheading").hide();
+        $("#new").hide();
+        $("#signinuser").show(); 
+    });
+
+    $("#signinuser").click(function(){ $("#loginheading").show();  
+    $("#signupform").hide(); 
+    $("#signupheading").hide();
+    $("#signinuser").hide();  
+    $("#new").show();
+  });
+  
+//   $( ".accordintik" ).click(function() {
+//     //alert("hi")
+//     $( this ).toggleClass( "showtik" );
+//    });
+
+  $( ".accordintik" ).click(function() {
+    //alert("hi")
+    $( this ).toggleClass( "showtik" );
+   });
+
+    
+});
